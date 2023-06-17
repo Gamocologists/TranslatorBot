7aae08ac897be5034b91cabc6590f1732cd5e6ff
jaschamerle <jascha.merle@gmail.com>
2023-06-16 23:40:33 -0400

    feat: added documentation comments for Commands.cs

--- Core/Commands.cs
+++ Core/Commands.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace TranslatorBot;
 
+/// <summary>
+///     The class which contains all the commands and their parameters.
+/// </summary>
 public static class Commands
 {
+    /// <summary>
+    ///     The method which returns all the command names.
+    /// </summary>
+    /// <returns>
+    ///     A <see cref="List{T}"/> of <see cref="string"/>s containing all the command names.
+    /// </returns>
     public static List<string> GetCommandNames()
     {
         List<string> commandNames = new ();
@@ -14,6 +23,21 @@ public static class Commands
         return commandNames;
     }
 
+    /// <summary>
+    ///     The method which returns all the command parameter names.
+    /// </summary>
+    /// <param name="commandName">
+    ///     The name of the command to get the parameter names for.
+    /// </param>
+    /// <param name="noSpaces">
+    ///     Whether or not to return the parameter names without spaces.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="List{T}"/> of <see cref="string"/>s containing all the command parameter names.
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown when the command name is not recognized.
+    /// </exception>
     public static List<string> GetCommandParameterNames(string commandName, bool noSpaces = false)
     {
         if (noSpaces)
